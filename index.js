df7b1d9823db12f950cede9ba0e68d0dd09ade29
kidhri salim <salimkhidhri@gmail.com>
2022-12-05 15:15:05 +0300

    complitedassignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const btn = document.getElementById('input');
+function clickAlert() {
+    alert('I was clicked!');
+  }
+
 function addingEventListener() {
+    btn.addEventListener("click", clickAlert);
 }
+
+addingEventListener();
